85889545d4bceb2139c7f0819b44c849e7f49328
MScholtes <markusscholtes@hotmail.com>
2021-07-04 07:28:58 +0200

    Update to V1.0.2

--- Module/WebServer.psm1
+++ Module/WebServer.psm1
@@ -2,8 +2,8 @@
 .SYNOPSIS
 Powershell WebServer is a module that starts a webserver (without the need for IIS). 
 .NOTES
-Version: 1.0.1
-Date: 2021-02-12
+Version: 1.0.2
+Date: 2021-07-04
 Author: Markus Scholtes
 #>
 
